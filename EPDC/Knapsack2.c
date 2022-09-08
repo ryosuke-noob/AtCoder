2bef4de6b2d8deb2c88e79f536b8091e079f10dc
rnutaba <sakana1484@gmail.com>
2022-09-08 15:49:32 +0900

    Knapsack2.c

--- EPDC/Knapsack2.c
+++ EPDC/Knapsack2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <limits.h>
 #define rep(i,l,r) for(int i=(l);i<(r);i++)
 #define max(p,q) ((p)>(q)?(p):(q))
 #define min(p,q) ((p)<(q)?(p):(q))
@@ -16,10 +17,10 @@ int main(void)
 	double **dp;
 
 	max_value = 100000;
-	scanf("%d %d",&N, &W);
+	(void)scanf("%d %d",&N, &W);
 	w = (int *)malloc(sizeof(int) * (N + 1));
 	v = (int *)malloc(sizeof(int) * (N + 1));
-	rep(i,1,N + 1) scanf("%d %d",&w[i],&v[i]);
+	rep(i,1,N + 1) (void)scanf("%d %d",&w[i],&v[i]);
 	dp = (double **)malloc(sizeof(double *) * (N + 1));
 	rep(i,0,N + 1) dp[i] = (double *)malloc(sizeof(double) * (max_value + 1));
 	rep(i,0,N + 1) rep(j,0,max_value + 1) dp[i][j] = INT_MAX;
