1a7af48342f0c839df1326f39d57c0334a0dd12c
rnutaba <sakana1484@gmail.com>
2022-09-13 22:00:49 +0900

    M-Candies 失敗作その3

--- EPDC/M-Candies.c
+++ EPDC/M-Candies.c
@@ -11,26 +11,31 @@ int main(void)
 {
 	int n,k;
 	int *a;
-	long int **dp;
+	unsigned long long **dp;
+	unsigned long long **dp2;
 
 	scanf("%d %d",&n,&k);
 	a = (int *)malloc(sizeof(int)*(n+1));
 	rep(i,1,n+1) scanf("%d",&a[i]);
-	dp = (long int **)malloc(sizeof(long int *) * (n+1));
-	rep (i,0,n+1) dp[i] = (long int *)malloc(sizeof(long int) * (k+1));
-	rep(i,0,k+1) rep(j,0,n+1) dp[j][i] = 0;
-	rep(i,0,n+1) dp[i][0] = 1;
+	dp = (unsigned long long **)malloc(sizeof(unsigned long long *) * (n+1));
+	rep (i,0,n+1) dp[i] = (unsigned long long *)malloc(sizeof(unsigned long long) * (k+1));
+	dp2 = (unsigned long long **)malloc(sizeof(unsigned long long *) * (n+1));
+	rep (i,0,n+1) dp2[i] = (unsigned long long *)malloc(sizeof(unsigned long long) * (k+1));
+	rep(i,0,k+1) {rep(j,0,n+1) {dp[j][i] = 0; dp2[j][i] = 0;} dp2[0][i] = 1;}
+	rep(i,0,n+1) {dp[i][0] = 1; dp2[i][0] = 1;}
 
 	rep(i,1,n+1)
 	{
 		rep(j,1,k+1)
 		{
-			rep(l,0,a[i]+1)
-			{
-				if (l <= j) dp[i][j] += dp[i - 1][j - l];
-			}
+			if (j >= a[i]) dp[i][j] = (dp2[i - 1][j] - dp2[i - 1][j - a[i]] + dp[i - 1][j - a[i]])% 1000000007;
+			else dp[i][j] = dp2[i-1][j];
+			dp2[i][j] = (dp[i][j] + dp2[i][j - 1]);
 		}
 	}
-	printf("%ld\n",dp[n][k] % (1000000007));
+	// rep(i,0,n+1) {rep(j,0,k+1) {printf("%2ld ",dp[i][j]);} printf("\n");}
+	// printf("\n");
+	// rep(i,0,n+1) {rep(j,0,k+1) {printf("%2ld ",dp2[i][j]);} printf("\n");}
+	printf("%.0lf\n", (double)dp[n][k]);
 	return (0);
 }
