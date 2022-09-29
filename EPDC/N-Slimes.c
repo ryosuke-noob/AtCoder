fe1cad3b0428c019316271b847ac42ff64edfe92
rnutaba <sakana1484@gmail.com>
2022-09-29 16:01:10 +0900

    N-Slimes fin

--- EPDC/N-Slimes.c
+++ EPDC/N-Slimes.c
@@ -7,102 +7,33 @@
 #define max(p,q) ((p)>(q)?(p):(q))
 #define min(p,q) ((p)<(q)?(p):(q))
 
-typedef struct s_node
+unsigned long long f(int l, int r, unsigned long long **dp, int *a)
 {
-	unsigned long long data;
-	struct s_node *next;
-} t_node;
-
-void input_data(t_node *link, int *a, int size)
-{
-	t_node *tmp;
-
-	tmp = link;
-	rep(i,1,size+1)
-	{
-		t_node *new = (t_node*)malloc(sizeof(t_node));
-		new->data = a[i];
-		new->next = 0;
-		tmp->next = new;
-		tmp = new;
-	}
-}
-
-t_node *create_elem(unsigned long long new_data)
-{
-	t_node *new = (t_node*)malloc(sizeof(t_node));
-
-	new->data = new_data;
-	new->next = 0;
-	return (new);
-}
-
-void delete_next_elem(t_node *pre_obj)
-{
-	t_node *obj;
-
-	obj = pre_obj->next;
-	pre_obj->next = obj->next;
-	free(obj);
+	if (dp[l][r]) return dp[l][r];
+	if (l==r) return 0;
+	unsigned long long fans = LLONG_MAX;
+	rep(m,l,r) fans = min(fans, f(l,m,dp,a)+f(m+1,r,dp,a));
+	dp[l][r] += fans;
+	rep(i,l,r+1) dp[l][r] += a[i];
+	return dp[l][r];
 }
 
 int main(void)
 {
 	int n;
 	int *a;
-	unsigned long long Min;
+	unsigned long long **dp;
 	unsigned long long ans = 0;
-	t_node *link, *dp_link;
-	t_node *tmp, *tmp2;
 
 	if(scanf("%d",&n) != 1) return 0;
 	a = (int*)malloc(sizeof(int)*(n+1));
 	rep(i,1,n+1) if(scanf("%d",&a[i]) != 1) return 0;
-	link = (t_node*)malloc(sizeof(t_node));
-	input_data(link,a,n);
+	dp = (unsigned long long**)malloc(sizeof(unsigned long long*)*(n+1));
+	rep(i,0,n+1) dp[i] = (unsigned long long*)malloc(sizeof(unsigned long long)*(n+1));
+	rep(i,0,n+1) rep(j,0,n+1) dp[i][j] = 0;
 
-	while (link->next->next != 0)
-	{
-		Min = LLONG_MAX;
-		//calculate min
-		tmp = link->next;
-		while (tmp != 0 && tmp->next != 0)
-		{
-			Min = min(Min, tmp->data + tmp->next->data);
-			tmp = tmp->next;
-		}
+	ans = f(1,n,dp,a);
 
-		//find min
-		tmp = link;
-		while (tmp != 0 && tmp->next != 0)
-		{
-			if (Min == tmp->data + tmp->next->data)
-			{
-				tmp->data = Min;
-				ans += Min;
-				delete_next_elem(tmp);
-				break ;
-			}
-			tmp = tmp->next;
-		}
-
-		// tmp = link->next;
-		// while(tmp != 0)
-		// {
-		// 	printf("%I64d ",tmp->data);
-		// 	tmp = tmp->next;
-		// }
-		// printf("\n%I64d\n",Min);
-	}
-
-	// tmp = link->next;
-	// while(tmp != 0)
-	// {
-	// 	printf("%I64d ",tmp->data);
-	// 	tmp = tmp->next;
-	// }
-	// printf("\n%I64d\n",Min);
 	printf("%I64lld\n",ans);
-
 	return (0);
 }
